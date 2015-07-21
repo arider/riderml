a237d21154780f05848d9d2a51f97f1117246915
arider <andrew.rider@gmail.com>
2015-07-21 15:57:19 -0700

    initial setup for pip installing.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='riderml',
+    version='0.1.0',
+    author=u'Andrew Rider',
+    author_email='andrew.rider',
+    packages=['numpy'],
+    url='',
+    license='MIT licence, see LICENCE.txt',
+    description='Assorted machine learning algorithms',
+    long_description=open('README.txt').read(),
+    zip_safe=False,
+)

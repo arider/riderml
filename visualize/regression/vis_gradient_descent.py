03035bad46f5e623c2be814431787676e9a5fd34
arider <andrew.rider@gmail.com>
2016-02-18 19:24:42 -0800

    fixed import

--- visualize/regression/vis_gradient_descent.py
+++ visualize/regression/vis_gradient_descent.py
@@ -1,6 +1,6 @@
 import numpy
 from ...regression.gradient_descent import gradient_descent
-from ...util.linear_functions import linear, dlinear
+from ...util.loss_functions import linear, dlinear
 
 from bokeh.models import Plot
 from bokeh.plotting import figure, output_file, show
@@ -54,9 +54,11 @@ if __name__ == "__main__":
         '#4444dd',
         '#3b0cbd',
     ]
+    HEX_RAINBOW.reverse()
 
 
-    f = figure(title="Gradient descent example", x_axis_label='x', y_axis_label='y')
+
+    f = figure(title="", x_axis_label='x', y_axis_label='y', x_range=[0, 500])
     output_file("gradient_descent.html")
     f.circle(train_x[:, 1], train_y[:, 0], legend='training data', color='#ff0000', alpha=.5)
     f.circle(test_x[:, 1], test_y[:, 0], legend='test data', color='#3300ff')
@@ -64,7 +66,8 @@ if __name__ == "__main__":
     for i in range(p_its):
         f.line(test_x[:, 1], predictions[i][:, 0],
                legend='iteration {}'.format(i * n_iterations),
-               color=HEX_RAINBOW[i])
+               color=HEX_RAINBOW[i],
+               line_width=3)
     f.legend.orientation = "top_left"
     show(f)
 
